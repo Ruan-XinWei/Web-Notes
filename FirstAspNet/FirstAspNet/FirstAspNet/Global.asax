ea61c65e801659eb11677693c54295984ae84381
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-09-29 23:44:52 +0800

    添加第一个AspNet项目

---
+++ FirstAspNet/FirstAspNet/FirstAspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstAspNet.MvcApplication" Language="C#" %>
---
+++ FirstAspNet/FirstAspNet/FirstAspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstAspNet.Startup))]
+namespace FirstAspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
