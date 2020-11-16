a2484cfbc8662e910083d2b7a2edbd68fffce736
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-11-17 00:25:14 +0800

    修改布局，添加字符验证码验证，调整圆角以及完善input输入问题

--- 实验2/scripts/main.js
+++ 实验2/scripts/main.js
@@ -1,3 +1,8 @@
+var submit = document.getElementById("submit")
+var char_code = document.getElementById("char-input")
+var refresh = document.getElementById("refresh")
+var string = "" //字符验证码
+
 var slider = document.getElementsByClassName("slider")[0]
 var text = document.getElementsByClassName("text")[0]
 var button = document.getElementsByClassName("button")[0]
@@ -21,15 +26,17 @@ var success_position_y = Math.round(Math.random() * 150 + 50) //y轴
 
 //产生一个四位验证码
 function makeCode() {
+    string = ""
     var code = document.getElementById("code-main")
     code.onclick = makeCode
-    var string = ""
+    refresh.onclick = makeCode
     for(let i = 0; i < 4; ++i) {
         string += createChar()
     }
     code.innerHTML = string
 }
 
+//随机产生一个字符a-z,A-Z,0-9
 function createChar() {
     var num = Math.round(Math.random()) == 0
     if(num == true) {
@@ -44,11 +51,40 @@ function createChar() {
     }
 }
 
+function listenDown(e) {
+    var e = e || window.event || e.which;
+    if(e.keyCode == 13) {
+        // alert("Success1")
+        testCode()
+    }
+    // return false;
+}
+
+//提交事件
+submit.onclick = testCode
+
+function testCode() {
+    // console.log("success")
+    console.log(char_code.value)
+    if (char_code.value == string) {
+        alert("字符验证码验证成功！")
+    } else {
+        alert("字符验证码验证失败！")
+    }
+    location.reload()
+}
+
+char_code.onkeypress = listenDown
+// onkeypress = " if(event.keyCode==13) { window.alert('触发事件，处理事情'); return false;}"
+
 // console.log("slider x" + slider.offsetWidth)
 // console.log("button x" + button.offsetWidth)
 
-button.onmousedown = buttonDown;
+//button.onmousedown = buttonDown
+//section.onmousedown = null
+icon.onmousedown = buttonDown
 
+//图片button点击事件
 function buttonDown(e) {
     button.style.transition = ""
     background.style.transition = ""
@@ -61,17 +97,19 @@ function buttonDown(e) {
     console.log("success_length = " + success_length)
 }
 
+//图片button移动事件
 function buttonMove(e) {
     var button_move = e.clientX
     move_length = moveLength(button_move - start_button_position, 0, max_length)
 
     button.style.left = move_length + "px"
-    background.style.width = move_length + "px"
+    background.style.width = move_length + 20 + "px"
 
     // console.log("success_length = " + success_length)
     // console.log("move_length = " + move_length)
 }
 
+//图片button鼠标松开
 function buttonUp() {
     var abs = Math.abs(move_length - success_length)
     console.log("abs = " + abs)
@@ -88,6 +126,7 @@ function buttonUp() {
     document.onmouseup = null
 }
 
+//返回一个符合规范的移动长度
 function moveLength(length, min, max) {
     if (length < min) {
         return min
@@ -97,6 +136,7 @@ function moveLength(length, min, max) {
     return length
 }
 
+//成功对齐
 function successMove() {
     success = true
     text.innerHTML = "解锁成功"
@@ -108,13 +148,15 @@ function successMove() {
     document.onmousemove = null
 
     setTimeout(function() {
-        alert("解锁成功")
+        alert("图片解锁成功！")
         location.reload()
     }, 100)
 }
 
 // console.log("success_position_x = " + success_position_x)
 // console.log("success_position_y = " + success_position_y)
+
+//随机挑选图片中的一部分
 function selectSection() {
     var img = document.getElementById("img")
     var height_string = img.getAttribute("height")
@@ -125,11 +167,12 @@ function selectSection() {
     // console.log("height = " + img_height)
     success_section.style.top = success_position_y + "px"
     success_section.style.left = success_position_x + "px"
-    section.style.top = success_position_y - img_height - 3 + "px"
+    section.style.top = success_position_y - img_height - 6 + "px"
     section.style.background = "url(" + img_url + ") -" + success_position_x + "px -" + success_position_y + "px/auto " + img_height + "px"
     // background: url(./img/img1.jpg) 100px 200px;  
 }
 
+//随机产生图片地址
 function randomImage() {
     var img = document.getElementById("img")
     var num = Math.ceil(Math.random()*5) + 1;
