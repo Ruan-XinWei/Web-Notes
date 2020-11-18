cc0c1ec80e73289a9a26c06636d62987d1537472
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-11-18 21:16:10 +0800

    完成MVCTest

--- MVCTest/MVCTest/Controllers/UserController.cs
+++ MVCTest/MVCTest/Controllers/UserController.cs
@@ -7,6 +7,7 @@ namespace MVCTest.Controllers
     public class UserController : Controller
     {
         static List<User> list = new List<User>();
+        static string username;
         // GET: User
         public ActionResult Enroll()
         {
@@ -17,6 +18,8 @@ namespace MVCTest.Controllers
         public ActionResult Enroll(User user)
         {
             list.Add(user);
+            list.Remove(null);
+            username = user.username;
             if (user.username == "admin")
             {
                 return View("Show", list);
@@ -24,14 +27,35 @@ namespace MVCTest.Controllers
             return View("Show_Common", user);
         }
 
-        public ActionResult Show()
+        [HttpPost]
+        public ActionResult Modify(User user, int new_id, string new_username, string new_password)
         {
-            return View("Show_Common");
+            list.Remove(user);
+            user = list.Find(u => u.id == user.id);
+            if (new_id != 0)
+            {
+                user.id = new_id;
+            }
+            if (new_username != "")
+            {
+                user.username = new_username;
+            }
+            if (new_password != "")
+            {
+                user.password = new_password;
+            }
+            list.Add(user);
+            return View("Show_Common", user);
         }
 
-        public ActionResult Modify(User user)
+        [HttpPost]
+        public ActionResult Delete(int id)
         {
-            return View(user);
+            if (list.Remove(list.Find(user => user.id == id)))
+            {
+                return View("Show", list);
+            }
+            return View("Error");
         }
     }
 }
